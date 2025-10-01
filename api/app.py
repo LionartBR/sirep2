5dc1bb73d28addf427e063888bf8e246d3c403d3
LionartBR <lionart.arthur@gmail.com>
2025-09-30 21:14:11 -0300

    Redirect root to login page

--- api/app.py
+++ api/app.py
@@ -47,7 +47,7 @@ def create_app() -> FastAPI:
 
     @app.get("/", include_in_schema=False)
     async def root() -> RedirectResponse:
-        return RedirectResponse(url="/app/")
+        return RedirectResponse(url="/app/login.html")
 
     return app
 
